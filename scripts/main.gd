43b8bccfc52a734a2de33227cb1259d40785cae1
QualityHardwareStore <149911612+QualityHardwareStore@users.noreply.github.com>
2026-06-27 19:54:30 +0100

    asdf

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,12 +3,15 @@ extends Node3D
 @onready var bg_image = %background_sprite
 @onready var bg_geo = $BG_Geo/SubViewport2/background
 @onready var maincam = %maincam
-@onready var obj3d = %"3d_objects"
+@onready var player = %player
 
-@onready var doorcol = $BG_Geo/SubViewport2/background/FLOOR/doorcol
+@onready var doorcol = $"BG_Geo/SubViewport2/background colliders/Walls/doorcol"
 
 # preload background assets here?
 #I also might not even have to do this, because the animated sprite 2d can store many background images!
+
+@onready var bg2geo = $"BG_Geo/SubViewport2/3d filter/SubViewport/3d_objects/bg2"
+
 
 # array of camera positions here for respective backgrounds
 # dictionary? camera position + background image + background geometry
@@ -17,14 +20,24 @@ extends Node3D
 
 # iterate through it cleanly = array ? And then each entry in the array is a dictionary
 
-var bgarray = [
-	{"campos" : Vector3(8.236,0.688,3.781), "camrot" : Vector3(12.0, 62.7, 0.0), "bgimage" : "bg1"}
-	
+@onready var geo_array: Array[Node3D] = [
+	null,
+	$"BG_Geo/SubViewport2/3d filter/SubViewport/3d_objects/bg2",
 ]
+
+# consider making this dict V @onready and then just adding the geo nodes to it instead maybe. I think it wasnt working earlier simply because it wasnt @onready lol
+
+var bgarray = [
+	{"campos" : Vector3(8.236,0.688,3.781), "camrot" : Vector3(12.0, 62.7, 0.0), "bgimage" : "bg1"},
+	{"campos" : Vector3(-8.907,4.349,-4.713), "camrot" : Vector3((79.6 - 90), -105.5, 0.0), "bgimage" : "bg2doorclose"},
+	{"campos" : Vector3(-8.907,4.349,-4.713), "camrot" : Vector3((79.6 - 90), -105.5, 0.0), "bgimage" : "bg2doorclose"},
+]
+
 
 var scene:int = 0
 
 func _ready() -> void:
+	
 	print(bgarray[scene])
 	var n:int = 0
 	for i in bgarray:
@@ -46,4 +59,13 @@ func changescene(i:int):
 	maincam.position = bgarray[i]["campos"]
 	maincam.rotation_degrees = bgarray[i]["camrot"]
 	bg_image.play(bgarray[i]["bgimage"])
+	if geo_array[i] != null:
+		if geo_array[i-1] != null:
+			geo_array[i-1].visible = false
+		geo_array[i].visible = true
 	Global.scenechange.emit()
+
+
+func scenetransition(body: CharacterBody3D, scene:int) -> void:
+	print("this has worked")
+	changescene(scene)
